cc6c15f83bca9abeea728aee32914be48bbc9913
mfofana <mfofana@localhost>
2024-02-16 13:22:33 -0500

    Add git ignore

--- src/main/java/patternmatching/PatternMatchingSwitch.java
+++ src/main/java/patternmatching/PatternMatchingSwitch.java
@@ -13,23 +13,24 @@ public class PatternMatchingSwitch {
 		}
 	}
 
+	// Not available yet
 	// ou can rewrite this code to use a pattern switch expression as follows:
-	public static double getPerimeterPattern(ShapeRecord shape) throws IllegalArgumentException {
-		return switch (shape) {
-		case RectangleRecord r -> 2 * r.length() + 2 * r.width();
-		case CircleRecord c    -> 2 * c.radius() * Math.PI;
-		default          -> throw new IllegalArgumentException("Unrecognized shape");
-		};
-	}
+//	public static double getPerimeterPattern(ShapeRecord shape) throws IllegalArgumentException {
+//		return switch (shape) {
+//		case RectangleRecord r -> 2 * r.length() + 2 * r.width();
+//		case CircleRecord c    -> 2 * c.radius() * Math.PI;
+//		default          -> throw new IllegalArgumentException("Unrecognized shape");
+//		};
+//	}
 
 	// The following example uses a switch statement instead of a switch expression:
-	public static double getPerimeterStatement(ShapeRecord shape) throws IllegalArgumentException {
-		switch (shape) {
-		case RectangleRecord r: return 2 * r.length() + 2 * r.width();
-		case CircleRecord c:    return 2 * c.radius() * Math.PI;
-		default:          throw new IllegalArgumentException("Unrecognized shape");
-		}
-	}
+//	public static double getPerimeterStatement(ShapeRecord shape) throws IllegalArgumentException {
+//		switch (shape) {
+//		case RectangleRecord r: return 2 * r.length() + 2 * r.width();
+//		case CircleRecord c:    return 2 * c.radius() * Math.PI;
+//		default:          throw new IllegalArgumentException("Unrecognized shape");
+//		}
+//	}
 }
 
 interface ShapeRecord {}
